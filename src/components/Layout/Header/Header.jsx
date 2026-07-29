ab74083b7a18d374f9233b8f829a483c4bd49022
HOLYdave_studios <HOLYdave.com>
2026-07-29 22:35:22 +0100

    feat: implement initial application header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -3,11 +3,48 @@ import './Header.css';
 function Header() {
   return ( 
     <header className='header'>
-      <div className="header-left">
-        
+      <div className="header__left">
+        <button className="sidebar__toggle" aria-label='toggle sidebar'>
+          <i className="material-icons">menu</i>
+        </button>
+        <a href="#" className="logo">
+          <img 
+            src="/youtube-image.pn" alt="Youtube-logo" 
+            loading='lazy'
+          />
+        </a>
+      </div>
+
+      <div className="header__center">
+          <form className="header__search-form">
+            <input 
+              className="header__search-input"
+              type='search videos'
+              placeholder='search'
+            />
+            <button className="header__search-button" aria-label='search'>
+              <i className="material-icons">search</i>
+            </button>
+          </form>
+          <button className="voice__search-button" aria-label='voice search'>
+            <i className="material-icons">mic</i>
+          </button>
+      </div>
+
+      <div className="header__right">
+        <button className="create-button" aria-label='create'>
+          <i className="material-icons">add</i>
+          <span>Create</span>
+        </button>
+        <button className="notification-button" aria-label='profile'>
+          <i className="material-icons">notifications</i>
+        </button>
+        <button className="profile-button" aria-label='profile'>
+          <i className="material-icons">account_circle</i>
+        </button>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header
