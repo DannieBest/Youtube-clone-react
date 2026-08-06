0612b9b4083e5a35b9ce7e8984fde377f4608c48
HOLYdave_studios <HOLYdave.com>
2026-08-06 21:03:05 +0100

    feat: create initial video data source

---
+++ src/data/video.js
@@ -0,0 +1,140 @@
+import thumbnail_1 from '../assets/thumbnails/thumbnail-1.webp';
+import thumbnail_2 from '../assets/thumbnails/thumbnail-2.webp';
+import thumbnail_3 from '../assets/thumbnails/thumbnail-3.webp';
+import thumbnail_4 from '../assets/thumbnails/thumbnail-4.webp';
+import thumbnail_5 from '../assets/thumbnails/thumbnail-5.webp';
+import thumbnail_6 from '../assets/thumbnails/thumbnail-6.webp';
+import thumbnail_7 from '../assets/thumbnails/thumbnail-7.webp';
+import thumbnail_8 from '../assets/thumbnails/thumbnail-8.webp';
+import thumbnail_9 from '../assets/thumbnails/thumbnail-9.webp';
+import thumbnail_10 from '../assets/thumbnails/thumbnail-10.webp';
+import thumbnail_11 from '../assets/thumbnails/thumbnail-11.webp';
+import thumbnail_12 from '../assets/thumbnails/thumbnail-12.webp';
+
+
+import channel_1 from '../assets/channel-images/channel-1.jpeg';
+import channel_2 from '../assets/channel-images/channel-2.jpeg';
+import channel_3 from '../assets/channel-images/channel-3.jpeg';
+import channel_4 from '../assets/channel-images/channel-4.jpeg';
+import channel_5 from '../assets/channel-images/channel-5.jpeg';
+import channel_6 from '../assets/channel-images/channel-6.jpeg';
+import channel_7 from '../assets/channel-images/channel-7.jpeg';
+import channel_8 from '../assets/channel-images/channel-8.jpeg';
+import channel_9 from '../assets/channel-images/channel-9.jpeg';
+import channel_10 from '../assets/channel-images/channel-10.jpeg';
+import channel_11 from '../assets/channel-images/channel-11.jpeg';
+import channel_12 from '../assets/channel-images/channel-12.jpeg';
+
+const videos = [
+  {
+    id: 1,
+    title: "Talking Tech and AI with Google CEO Sundar Pichai!",
+    channel: "Marques Brownlee",
+    thumbnail: thumbnail_1,
+    channelImage: channel_1,
+    views: "3.4M views",
+    uploaded: "6 months ago",
+  },
+  {
+    id: 2,
+    title: "Try Not To Laugh Challenge #9",
+    channel: "Markiplier",
+    thumbnail: thumbnail_2,
+    channelImage: channel_2,
+    views: "19M views",
+    uploaded: "4 years ago",
+  },
+  {
+    id: 3,
+    title: "Crazy Tik Toks Taken Moments Before DISASTER",
+    channel: "SSSniper Wolf",
+    thumbnail: thumbnail_3,
+    channelImage: channel_3,
+    views: "12M views",
+    uploaded: "1 year ago",
+  },
+  {
+    id: 4,
+    title: "The Simplest Math Problem No One Can Solve - Collatz Conjecture",
+    channel: "Veritasium",
+    thumbnail: thumbnail_4,
+    channelImage: channel_4,
+    views: "18M views",
+    uploaded: "4 months ago",
+  },
+  {
+    id: 5,
+    title: "Kadane's Algorithm To Maximum Sum Subarray Problem",
+    channel: "CS Dojo",
+    thumbnail: thumbnail_5,
+    channelImage: channel_5,
+    views: "519k views",
+    uploaded: "5 years ago",
+  },
+  {
+    id: 6,
+    title: "Anything You can Fit In This Circle I'll Pay For",
+    channel: "MrBeast",
+    thumbnail: thumbnail_6,
+    channelImage: channel_6,
+    views: "141M views",
+    uploaded: "1 year ago",
+  },
+  {
+    id: 7,
+    title: "Why Planes Don't Fly Over Tibet",
+    channel: "RealLifeLore",
+    thumbnail: thumbnail_7,
+    channelImage: channel_7,
+    views: "6.6M views",
+    uploaded: "1 year ago",
+  },
+  {
+    id: 8,
+    title: "Inside The World's Biggest Passenger Plane",
+    channel: "Tech Vision",
+    thumbnail: thumbnail_8,
+    channelImage: channel_8,
+    views: "3.7M views",
+    uploaded: "10 months ago",
+  },
+
+  {
+    id: 9,
+    title: "The SECRET to Super Human STRENGTH",
+    channel: "ThenX",
+    thumbnail: thumbnail_9,
+    channelImage: channel_9,
+    views: "20M views",
+    uploaded: "3 years ago",
+  },
+  {
+    id: 10,
+    title: "How the world's Largest Cruise Ship Makes 30,000 Meals Every Day",
+    channel: "Business Insider",
+    thumbnail: thumbnail_10,
+    channelImage: channel_10,
+    views: "14M views",
+    uploaded: "1 year ago",
+  },
+  {
+    id: 11,
+    title: "Dubai's Crazy Underwater Train and Other Things #Only in Dubai",
+    channel: "Destination Tips.",
+    thumbnail: thumbnail_11,
+    channelImage: channel_11,
+    views: "3M views",
+    uploaded: "1 year ago",
+  },
+  {
+    id: 12,
+    title: "What would happen if you didn't drink water?- Mia Nacamulli",
+    channel: "TED-Ed",
+    thumbnail: thumbnail_12,
+    channelImage: channel_12,
+    views: "12M views",
+    uploaded: "5 years ago",
+  }
+];
+
+export default videos;
